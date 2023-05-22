d06212d7ac75902ddfc1ba54851b83f67ddd3292
szrnx <34532700+ysgraph@users.noreply.github.com>
2023-05-22 22:55:27 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `portfolio-2023`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
